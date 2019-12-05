f09609fc996793830487dd46e7c87d5cea7a1422
chirsz <chirsz@foxmail.com>
2019-12-05 23:32:32 +0800

    增加注释

--- src/nowebasewindow.h
+++ src/nowebasewindow.h
@@ -1,6 +1,9 @@
 #ifndef NOWEBASEWINDOW_H
 #define NOWEBASEWINDOW_H
 
+// Nowe 项目中统一风格的基类，取消系统标题栏、使用自定义风格的最小化和关闭按钮，允许使用鼠标拖动窗口。
+// 注意：请在 setUp(ui) 后调用 NoweBaseWindow::initNoweStyle() 以实现它的功能。
+
 #include <QWidget>
 
 class NoweBaseWindow : public QWidget

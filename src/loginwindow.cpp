f75be8ef90dc628873bcb56ff22a68e9d5841047
radoth <activeiy@icloud.com>
2019-12-07 22:09:01 +0800

    重新设计登录页面，增加动画
    重新设计登录页面，增加动画

--- src/loginwindow.cpp
+++ src/loginwindow.cpp
@@ -6,6 +6,9 @@
 #include <QMessageBox>
 #include <QXmppVCardManager.h>
 #include <QCloseEvent>
+#include <QPropertyAnimation>
+#include <QGraphicsOpacityEffect>
+#include <QTimer>
 
 LoginWindow::LoginWindow(QWidget *parent) :
     NoweBaseWindow(parent),
@@ -40,6 +43,9 @@ LoginWindow::LoginWindow(QWidget *parent) :
         QMessageBox::critical(this, tr("登陆失败"), tr("登陆过程中出错"));
     });
 
+    QGraphicsOpacityEffect * m_pGraphicsOpacityEffect1=new QGraphicsOpacityEffect(ui->ingLabel);
+    m_pGraphicsOpacityEffect1->setOpacity(0);
+    ui->ingLabel->setGraphicsEffect(m_pGraphicsOpacityEffect1);
 }
 
 LoginWindow::~LoginWindow()
@@ -80,6 +86,51 @@ void LoginWindow::Login()
 {
     QString jid = ui->jidLnEdt->text();
     QString passwd = ui->passwdLnEdt->text();
+
+    QPropertyAnimation *anime=new QPropertyAnimation(ui->loginLabel,"geometry");
+    anime->setDuration(1000);
+    anime->setStartValue(QRect(30,10,61,61));
+    anime->setEndValue(QRect(91,35,61,61));
+    anime->setEasingCurve(QEasingCurve::OutCubic);
+    anime->start();
+
+    QPropertyAnimation *anime2=new QPropertyAnimation(ui->ingLabel,"geometry");
+    anime2->setDuration(1000);
+    anime2->setStartValue(QRect(-31,10,61,61));
+    anime2->setEndValue(QRect(30,35,61,61));
+    anime2->setEasingCurve(QEasingCurve::OutCubic);
+    anime2->start();
+
+    QGraphicsOpacityEffect * m_pGraphicsOpacityEffect1=new QGraphicsOpacityEffect(ui->ingLabel);
+    m_pGraphicsOpacityEffect1->setOpacity(0);
+    ui->ingLabel->setGraphicsEffect(m_pGraphicsOpacityEffect1);
+
+    QPropertyAnimation *anime3=new QPropertyAnimation(m_pGraphicsOpacityEffect1,"opacity");
+    anime3->setDuration(500);
+    anime3->setStartValue(0);
+    anime3->setEndValue(1);
+    anime3->setEasingCurve(QEasingCurve::OutCubic);
+    anime3->start();
+
+    QPropertyAnimation *anime4=new QPropertyAnimation(ui->colorBar,"geometry");
+    anime4->setDuration(500);
+    anime4->setStartValue(QRect(0,0,500,91));
+    anime4->setEndValue(QRect(0,-40,500,91));
+    anime4->setEasingCurve(QEasingCurve::OutCubic);
+    anime4->start();
+
+    QPropertyAnimation *anime5=new QPropertyAnimation(ui->mainFrame,"geometry");
+    anime5->setDuration(2000);
+    anime5->setStartValue(QRect(20,100,451,211));
+    anime5->setEndValue(QRect(20,100,451,211));
+    anime5->setKeyValueAt(0.5,QRect(20,500,451,211));
+    anime5->setKeyValueAt(0.9,QRect(20,500,451,211));
+    anime5->setEasingCurve(QEasingCurve::InCubic);
+    anime5->start();
+
+    QEventLoop eventloop;
+    QTimer::singleShot(700, &eventloop, SLOT(quit())); //wait 2s
+    eventloop.exec();
 
     QXmppConfiguration config;
     config.setJid(jid);
